9c2fe60fc65f98b859a23d20e28026bdc033da88
ahmadmamdouh <mamdouh486@gmail.com>
2020-02-18 16:44:20 +0200

    add img folder

--- js/app.js
+++ js/app.js
@@ -9,10 +9,17 @@ switch (guess1.toLowerCase()) {
         break;
     case "no":
         alert('try again bro');
-       // console.log(guess1);
+    // console.log(guess1);
+    case "y":
+        alert('you are great man ');
+        //console.log(guess1);
+        break;
+    case "n":
+        alert('try again bro');
+        // console.log(guess1);
         break;
     default:
-        //console.log(guess1);
+    //console.log(guess1);
 
 }
 alert(`first answer is ${guess1}`);
@@ -21,14 +28,22 @@ var guess2 = prompt("can you guess my fav food ?", "yes or no");
 switch (guess2.toLowerCase()) {
     case "yes":
         alert('good job man');
-       // console.log(guess2);
-        break
+        // console.log(guess2);
+        break;
     case "no":
         alert('opps man ! try once more ');
-       // console.log(guess2);
-        break
+        // console.log(guess2);
+        break;
+    case "y":
+        alert('good job man');
+        // console.log(guess2);
+        break;
+    case "n":
+        alert('opps man ! try once more ');
+        // console.log(guess2);
+        break;
     default:
-        //console.log(guess2);
+    //console.log(guess2);
 
 }
 alert(`second answer is ${guess2}`);
@@ -37,14 +52,22 @@ var guess3 = prompt("can you guess my age ?", "yes or no")
 switch (guess3.toLowerCase()) {
     case "yes":
         alert('amazing man !');
-       // console.log(guess3);
+        // console.log(guess3);
         break;
     case "no":
         alert('oppes , welcome any where !');
         //console.log(guess3);
         break;
+    case "y":
+        alert('amazing man !');
+        // console.log(guess3);
+        break;
+    case "n":
+        alert('oppes , welcome any where !');
+        //console.log(guess3);
+        break;
     default:
-       // console.log(guess3);
+    // console.log(guess3);
 
 }
 alert(`your third answer is : ${guess3}`);
@@ -52,14 +75,22 @@ var guess4 = prompt("can you guess my phone type ?", "yes or no")
 switch (guess4.toLowerCase()) {
     case "yes":
         alert('perfect man !');
-       // console.log(guess4);
+        // console.log(guess4);
         break;
     case "no":
         alert('oppes , take another chance !');
-       // console.log(guess4);
+        // console.log(guess4);
+        break;
+    case "y":
+        alert('perfect man !');
+        // console.log(guess4);
+        break;
+    case "n":
+        alert('oppes , take another chance !');
+        // console.log(guess4);
         break;
     default:
-        //console.log(guess4);
+    //console.log(guess4);
 
 }
 alert(`your name is ${name}`);
