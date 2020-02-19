a692d3c344155738833f9255f183b2e54278783b
dianaazzam <dianaazzam@DESKTOP-9F18RBI.localdomain>
2020-02-19 16:03:07 +0200

    added functions

--- js/app.js
+++ js/app.js
@@ -1,173 +1,224 @@
 'use strict';
-alert('welcome to my web site ');
+
+
+alert('welcome to my website ');
 var x = 0;
-var name = prompt("can you tell us your NAME pleas !", "ex:AHMAD");
-var guess1 = prompt("can you guess my studied ?", "yes or no");
-switch (guess1.toLowerCase()) {
-    case "yes":
-        alert('you are great man ');
-        x = x + 1;
-        //console.log(guess1);
-        break;
-    case "no":
-        alert('try again bro');
-    // console.log(guess1);
-    case "y":
-        alert('you are great man ');
-        x = x + 1;
-        //console.log(guess1);
-        break;
-    case "n":
-        alert('try again bro');
-        // console.log(guess1);
-        break;
-    default:
-    //console.log(guess1);
+var name = prompt("can you tell us your NAME please !", "ex:AHMAD");
 
-}
-alert(`first answer is ${guess1}`);
+function major() {
+    var guess1 = prompt("Do you think I studied Electrical engineering?");
+    switch (guess1.toLowerCase()) {
+        case "yes":
+        case "y":
+            alert('you are great man ');
+            x = x + 1;
+            //console.log(guess1);
+            break;
 
-var guess2 = prompt("can you guess my fav food ?", "yes or no");
-switch (guess2.toLowerCase()) {
-    case "yes":
-        alert('good job man');
-        x = x + 1;
-        // console.log(guess2);
-        break;
-    case "no":
-        alert('opps man ! try once more ');
-        // console.log(guess2);
-        break;
-    case "y":
-        alert('good job man');
-        x = x + 1;
-        // console.log(guess2);
-        break;
-    case "n":
-        alert('opps man ! try once more ');
-        // console.log(guess2);
-        break;
-    default:
-    //console.log(guess2);
+        case "no":
+        case "n":
+            alert('try again bro');
+            // console.log(guess1);
+            break;
+        default: guess1 = prompt("Only yes/y or no/n");
 
-}
-alert(`second answer is ${guess2}`);
-
-var guess3 = prompt("can you guess my age ?", "yes or no")
-switch (guess3.toLowerCase()) {
-    case "yes":
-        alert('amazing man !');
-        x = x + 1;
-        // console.log(guess3);
-        break;
-    case "no":
-        alert('oppes , welcome any where !');
-        //console.log(guess3);
-        break;
-    case "y":
-        alert('amazing man !');
-        x = x + 1;
-        // console.log(guess3);
-        break;
-    case "n":
-        alert('oppes , welcome any where !');
-        //console.log(guess3);
-        break;
-    default:
-    // console.log(guess3);
-
-}
-alert(`your third answer is : ${guess3}`);
-
-var guess4 = prompt("can you guess my phone type ?", "yes or no")
-switch (guess4.toLowerCase()) {
-    case "yes":
-        alert('perfect man !');
-        x = x + 1;
-        // console.log(guess4);
-        break;
-    case "no":
-        alert('oppes , take another chance !');
-        // console.log(guess4);
-        break;
-    case "y":
-        alert('perfect man !');
-        x = x + 1;
-        // console.log(guess4);
-        break;
-    case "n":
-        alert('oppes , take another chance !');
-        // console.log(guess4);
-        break;
-    default:
-    //console.log(guess4);
-
-}
-alert(`your fourth answer is : ${guess4}`);
-
-
-var guess5 = prompt("can you guess my car model ?", "yes or no")
-switch (guess5.toLowerCase()) {
-    case "yes":
-        alert('perfect man !');
-        x = x + 1;
-        // console.log(guess4);
-        break;
-    case "no":
-        alert('oppes , take another chance !');
-        // console.log(guess4);
-        break;
-    case "y":
-        alert('perfect man !');
-        x = x + 1;
-        // console.log(guess4);
-        break;
-    case "n":
-        alert('oppes , take another chance !');
-        // console.log(guess4);
-        break;
-    default:
-    //console.log(guess4);
-
-}
-alert(`your fifth answer is : ${guess5}`);
-
-
-var guess6 = prompt("can you guess my brothers num ? ", "guess anumber between 1 & 10")
-for (var i = 0; i < 3; i++) {
-    if (guess6 == 6) {
-        alert('you are geniuses bro');
-        x = x + 1;
-        { break; };
-    } else if (guess6 < 5) {
-        alert('the guess is too low , try agin ');
-        var guess6 = prompt("can you guess my brothers num ? ", "guess anumber between 1 & 10");
-    } else if (guess6 > 5) {
-        alert('the guess is near , try again ');
-        var guess6 = prompt("can you guess my brothers num ? ", "guess anumber between 1 & 10");
     }
+    alert(`first answer is ${guess1}`);
+
 }
-alert(`your sixth answer is : ${guess6}`);
-alert(`the correct answer is : 6 `)
-alert(`your name is ${name}`);
-//console.log(name);
+major();
 
-var laptobs = ['lenovo', 'hp', 'dell', 'ibm', 'acer', 'mac'];
-var guess7 = prompt("guess my fav laptob type ?", "dell");
 
-for (var i = 0; i < 5; i++) {
-    if (laptobs[i] == guess7 ) {
-        alert('good job bro !');
-        x = x + 1;
-        { break; };
-    } else {
-        alert('try agin man !');
-        var guess7 = prompt("guess my fav laptob type ?", "dell");
+
+function food() {
+    var guess2 = prompt("Do you think I like falafel?");
+    switch (guess2.toLowerCase()) {
+        case "yes":
+        case "y":
+            alert('good job man');
+            x = x + 1;
+            // console.log(guess2);
+            break;
+
+
+        case "n":
+        case "no":
+            alert('opps man ! try once more ');
+            // console.log(guess2);
+            break;
+
+        default: guess2 = prompt("Only yes/y or no/n");
+        //console.log(guess2);
+
     }
+    alert(`second answer is ${guess2}`);
+
+}
+food();
+
+
+
+function age() {
+    var guess3 = prompt("Do you think I'm above 20?", "yes or no")
+    switch (guess3.toLowerCase()) {
+        case "yes":
+        case "y":
+
+            alert('amazing man!');
+            x = x + 1;
+            // console.log(guess3);
+            break;
+        case "no":
+        case "n":
+
+            alert('oppes , welcome any where !');
+            //console.log(guess3);
+            break;
+
+        default: guess3 = prompt("Only yes/y or no/n");
+            break;
+
+
+        // console.log(guess3);
+
+    }
+    alert(`your third answer is : ${guess3}`);
+
 }
 
-alert(`the answer is : ${laptobs}`);
-alert(`the number of correct answer out of 7 is : ${x}`);
+age();
+
+function phone() {
+    var guess4 = prompt("Do think I have Iphone?")
+    switch (guess4.toLowerCase()) {
+        case "yes":
+        case "y":
+            alert('Opps I have Samsung!');
+            // console.log(guess4);
+            break;
+        case "no":
+        case "n":
+            x = x + 1;
+
+            alert("That's right. I have Samsung!");
+            // console.log(guess4);
+            break;
+
+        default: guess4 = prompt("Only yes/y or no/n");
+            break;
+        //console.log(guess4);
+
+    }
+    alert(`your fourth answer is : ${guess4}`);
+
+
+}
+
+phone();
+
+
+function car() {
+    var guess5 = prompt("Do you think I have Kia?")
+    switch (guess5.toLowerCase()) {
+        case "yes":
+        case "y":
+
+            alert('perfect man !');
+            x = x + 1;
+            // console.log(guess4);
+            break;
+
+        case "no":
+        case "n":
+
+            alert('oppes , take another chance !');
+            // console.log(guess4);
+            break;
+
+
+        default: guess5 = prompt("Only yes/y or no/n");
+            break;
+
+        //console.log(guess4);
+
+    }
+    alert(`your fifth answer is : ${guess5}`);
+
+}
+
+car();
+
+function siblings() {
+
+    var guess6 = parseInt(prompt("can you guess my brothers num ? ", "guess anumber between 1 & 10"));
+
+    for (var i = 0; i < 4; i++) {
+        if (guess6 === 6) {
+            alert('you are geniuses bro');
+            x = x + 1;
+             break;
+
+        } else if (guess6 < 5) {
+            alert('the guess is too low , try agin ');
+            var guess6 = parseInt(prompt("can you guess my brothers num ? ", "guess anumber between 1 & 10"));
+
+        } else if (guess6 > 5) {
+            alert('the guess is near , try again ');
+            var guess6 = parseInt(prompt("can you guess my brothers num ? ", "guess anumber between 1 & 10"));
+        }
+        else {
+            var guess6 = prompt("Put an integer");
+
+        }
+    }
+    alert(`your sixth answer is : ${guess6}`);
+    alert(`the correct answer is : 6 `)
+    alert(`your name is ${name}`);
+    //console.log(name);
+}
+
+siblings();
+
+
+
+function laptops() {
+    var laptops = ['lenovo', 'hp', 'dell', 'ibm', 'acer', 'mac'];
+
+    var test = false;
+
+    for (var j = 0; j < 7; j++) {
+
+        var guess7 = prompt("guess my fav laptop type ?", "dell");
+
+        for (var i = 0; i < laptops.length; i++) {
+            if (guess7=laptops[i]) {
+                alert('good job bro !');
+                x = x + 1;
+                { break; };
+            }
+            else {
+                alert('try agin man !');
+                var guess7 = prompt("guess my fav laptop type ?", "dell");
+            }
+        }
+        if (test);
+        break;
+
+    }
+
+
+
+
+    alert(`the answer is : ${laptops}`);
+    alert(`the number of correct answer out of 7 is : ${x}`);
+
+}
+
+laptops();
+
+
+
+
 
 
 
